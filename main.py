fdee786eec5343e3dc27174b87c857bcfdf732b1
KrishPatel <krishpatelsuper@gmail.com>
2022-10-22 13:45:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(21,GPIO.OUT)
-print("LED on")
-GPIO.output(21,GPIO.LOW)
-time.sleep(10)
-print("LED off")
-GPIO.output(21,GPIO.LOW)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6,GPIO.OUT)
+GPIO.output(5,GPIO.LOW)
+GPIO.output(6, GPIO.HIGH);
