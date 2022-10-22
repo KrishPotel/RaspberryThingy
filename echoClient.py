025932413f5eb24329f6dda3c5edf45cc577f334
KrishPatel <krishpatelsuper@gmail.com>
2022-10-22 17:58:10 -0400

    wefwf

---
+++ echoClient.py
@@ -0,0 +1,16 @@
+# echo-client.py
+
+from operator import truediv
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    while True:
+        data = s.recv(1024)
+        if data == b"W":
+            print("WOOOOOOOOOOOOOOOOO")    
+        print(f"Received {data!r}")
