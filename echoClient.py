816b11667961c571a88acdca48f2fc575cf8f524
KrishPatel <krishpatelsuper@gmail.com>
2022-10-23 19:16:57 -0400

    EFEFEF

--- echoClient.py
+++ echoClient.py
@@ -2,11 +2,11 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "192.168.68.163"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((socket.gethostname(), 8000))
+    s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
     while True:
         data = s.recv(1024)
