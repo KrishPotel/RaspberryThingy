fee518da2e3e40802e4825e0b5a36be8f439ee6c
KrishPatel <krishpatelsuper@gmail.com>
2022-10-23 20:33:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ GPIO.setup(6,GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 
-GPIO.output(5, GPIO.HIGH)
-GPIO.output(6, GPIO.LOW)
-
 HOST = "192.168.68.163"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
@@ -35,7 +32,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if t == True:
                 GPIO.output(5, GPIO.HIGH)
                 GPIO.output(6, GPIO.LOW)
-        
+            else:
+                GPIO.output(5, GPIO.LOW)
+                GPIO.output(6, GPIO.LOW)
             data = conn.recv(1024)
             if data == b"W":
                 print("W")
@@ -49,6 +48,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if data == b"A":
                 print("A")
             if data == b"stop":
+                t = False
                 GPIO.output(5, GPIO.LOW)
                 GPIO.output(6, GPIO.LOW)
 
